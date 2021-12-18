05c9a9475b1e14323b6bd5b6aa3ef9c4be07d578
Vikas Panwar <cs.panwar@gmail.com>
2021-12-18 17:47:10 +0530

    Custom Exceptions

---
+++ 03_ThreadsException/src/simplilearn/java/CustomException.java
@@ -0,0 +1,30 @@
+package simplilearn.java;
+
+class MyException2 extends Exception {
+	String str1;
+
+	public MyException2(String str2) {
+		str1 = str2;
+	}
+
+	@Override
+	public String toString() {
+		return ("MyException Occurred:" + str1);
+	}
+}
+
+public class CustomException {
+
+	public static void main(String[] args) {
+		try {
+			System.out.println("Starting of try block");
+			// I'm throwing the custom exception using throw
+			throw new MyException2("This is My error Message");
+			
+		} catch (Exception e) {
+			System.out.println("Catch Block");
+			System.out.println(e);
+		}
+	}
+
+}
